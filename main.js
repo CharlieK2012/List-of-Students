a2483518583591909aa2a2305e75794b91445ab5
CharlieK2012 <102316500+CharlieK2012@users.noreply.github.com>
2022-06-29 17:31:49 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var n =[];
+function submit(){
+var name_1 =document.getElementById("s1").value;
+var name_2 =document.getElementById("s2").value;
+var name_3 =document.getElementById("s3").value;
+var name_4 =document.getElementById("s4").value;
+n.push(name_1);
+n.push(name_2);
+n.push(name_3);
+n.push(name_4);
+document.getElementById("display").innerHTML=n;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
